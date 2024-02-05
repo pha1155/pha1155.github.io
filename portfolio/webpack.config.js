721b44ff2d0bd7146bfb3c5093aad896d1a64f47
nani <pha3113@naver.com>
2024-02-05 22:23:29 +0900

    feat - ejs setting
    ejs setting

--- portfolio/webpack.config.js
+++ portfolio/webpack.config.js
@@ -1,10 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/js/index.js',
-    output: {
-        filename: './js/index.bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    mode: 'development',
+	entry: './src/js/index.js',
+	output: {
+		filename: './js/index.bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'PARK HYOEN AH`s portfolio',
+			minify: {
+				collapseWhitespace: true
+			},
+			hash: true,
+			template: './src/index.ejs'
+		})
+	],
+	mode: 'development',
 };
